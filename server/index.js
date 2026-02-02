c50d7910b46e927e9d8560861a0539d020adb8e2
Sowndharluna <krs007sowndhar@gmail.com>
2026-02-02 21:26:01 +0530

    'at10'

--- server/index.js
+++ server/index.js
@@ -1,19 +1,28 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import bodyParser from 'body-parser';
-import cors from 'cors';
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+
+import postRoutes from './routes/posts.js';
 
 const app = express();
-  
-app.use(bodyParser.json({limit: "30mb",extended:true}));
-app.use(bodyParser.urlencoded({limit: "30mb",extended:true}));
+
+app.use('/posts',postRoutes);
+
+app.use(express.json({ limit: "30mb" }));
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-const CONNECTION_URL = 'mongodb+srv://krs007sowndhar_db_user:sowndharluna001@cluster0.ktymh4k.mongodb.net/'
+const CONNECTION_URL =
+  "mongodb+srv://krs007sowndhar_db_user:sowndharluna001@cluster0.ktymh4k.mongodb.net/";
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL,{useNewUrlParser:true,useUnifiedTopology:true})
-    .then (()=>app.listen(PORT,()=> console.log(`Server running on port : ${PORT}`)))
-    .catch((error)=> console.log(error.message));
-
-mongoose.set('useFindAndModify',false);
+mongoose
+  .connect(CONNECTION_URL)
+  .then(() => {
+    app.listen(PORT, () =>
+      console.log(`✅ Server running on port: ${PORT}`)
+    );
+  })
+  .catch((error) => {
+    console.error("❌ MongoDB connection error:", error.message);
+  });
